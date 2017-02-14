7d5990fdddc3c3333e97cc9c64f4bda4ad2cae9c
Nathan <nathanstarwalt@gmail.com>
2017-02-14 15:10:33 -0500

    recreated the toJSON method to reintigrate functionality

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,5 +7,5 @@ var likes = new like.Counter();
 $(".likes-button").on("click", function(){
   likes.increase();
   console.log(likes.get("count"));
-  $(".likes-count").text(likes.get("count") + likes.toJSON());
+  $(".likes-count").text(likes.get("count") + " " + likes.toJSON().string);
 });
--- app/scripts/models/like.js
+++ app/scripts/models/like.js
@@ -6,7 +6,9 @@ var Counter = Backbone.Model.extend({
     console.log("Model made");
   },
   defaults: {
-    count : 0
+    count : 0,
+    label: 'Like',
+    plural: 's'
   },
 
   increase: function(){
@@ -14,11 +16,17 @@ var Counter = Backbone.Model.extend({
     this.set({count : i});
   },
   toJSON: function(){
-    if (this.get("count") === 1 ){
-      return " Like";
-    } else {
-      return " Likes";
-    }
+    //From the review - 2-14-2017
+    var data = Backbone.Model.prototype.toJSON.call(this);
+    var j = this.get('count');
+    data.string = this.get('label') + ((j === 1) ? "" : this.get('plural'));
+    console.log(data);
+    return data;
+    // if (this.get("count") === 1 ){
+    //   return " Like";
+    // } else {
+    //   return " Likes";
+    // }
   }
 });
 
